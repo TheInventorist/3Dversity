0a1dccc96675dd754cdec207432af786dc4ab6dc
TheInventorist <vdepassier@gmail.com>
2019-09-10 06:38:39 -0400

    testing

--- 3Dversity/Assets/Scripts/login.cs
+++ 3Dversity/Assets/Scripts/login.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class login : MonoBehaviour{
+public class Login : MonoBehaviour{
 
     public GameObject email;
     public GameObject passwd;
     public GameObject btnLogin;
-     public GameObject logear;
+     //public GameObject logear;
      private string Email;
      private string Passwd;
 
@@ -17,37 +17,28 @@ public class login : MonoBehaviour{
     public Slider slider;
     //public Text progreso;
 
-    private void validarLogin(){
+    private int validarLogin(string Email, string Passwd){
         if((Email == "User1@gmail.com") && (Passwd ==  "admin")){
-            //StartCoroutine(CargaAsincronica(1));
-            SceneManager.LoadScene(1);
+            return 1;
         }
         else{
-            print("usuario o contraseña incorrecta");
+            return 0;
         }
     }
 
-   /*  IEnumerator CargaAsincronica(int indice){
-        AsyncOperation operacion = SceneManager.LoadSceneAsync(indice);
-        pantallaDeCarga.SetActive(true);
-
-        
-        while(!operacion.isDone){
-            float progreso = Mathf.Clamp01(operacion.progress / .9f);
-            slider.value = progreso;
-
-            yield return null;
-        }
-        
+    private void cargador(){
+        SceneManager.LoadScene(1);
     }
-*/
+
     void Update(){
         Email = email.GetComponent<InputField>().text;
         Passwd = passwd.GetComponent<InputField>().text;
-        //btnLogin = logear.GetComponent<Button>();
         
         UnityEngine.UI.Button btn = btnLogin.GetComponent<Button>();
         
-        btn.onClick.AddListener(validarLogin);
+        if(validarLogin(Email, Passwd) == 1){
+            btn.onClick.AddListener(cargador);
+        }
+        
     }
 }
---
+++ 3Dversity/Assets/Tests/LoginTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LoginTest
+    {
+        // A Test behaves as an ordinary method
+        //[TestCase("User1@gmail.com","admin")]
+        //[TestCase("User1@gmail.com","Admin")]
+        //[TestCase("User2@gmail.com","admin")]
+        //[TestCase("User3@gmail.com","Admin")]
+        [Test]
+        public void LoginTestSimplePasses(string mail, string contra){
+            // Use the Assert class to test conditions
+            var login = new Login();
+            
+            int puntaje = login.validarLogin(mail, contra);
+
+            Assert.That(1, Is.EqualTo(puntaje));
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator LoginTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
